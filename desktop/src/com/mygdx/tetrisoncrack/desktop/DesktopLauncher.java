1279a81e50388dd56ccf38790e255dd69c1f8b94
Randigtschackspel <randigtschackspel@gmail.com>
2016-04-27 09:42:36 +0200

    Added new code

---
+++ desktop/src/com/mygdx/tetrisoncrack/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.tetrisoncrack.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.tetrisoncrack.TetrisOnCrack;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TetrisOnCrack(), config);
+	}
+}
