1f2d9f3b5f17c93d936e82cbd243a61d772f8233
Randigtschackspel <randigtschackspel@gmail.com>
2016-05-05 14:50:35 +0200

    Implemented game states for pausing and a grid

--- core/src/com/mygdx/tetrisoncrack/Ass.java
+++ core/src/com/mygdx/tetrisoncrack/Ass.java
@@ -15,9 +15,9 @@ package com.mygdx.tetrisoncrack;
 
 public class Ass {
 
-    public static TextureRegion menuScreen , gameScreen, pauseScreen;
-    public static Rectangle menuScreenStartButton , gamePauseButton, pauseScreenMenuButton;
-
+    public static TextureRegion menuScreen , gameScreen, pauseScreen, blockTextureRegion;
+    public static Rectangle tetrisScreenGrid, menuScreenStartButton , gamePauseButton, pauseScreenMenuButton;
+    public static Color black;
 
     // Load assets
     public static void load(){
@@ -53,11 +53,68 @@ public class Ass {
         // May implement Stage and Actors?
         gamePauseButton = new Rectangle(390, 10, 80, 80);
 
+        // The area where the blocks are falling and stuff
+        tetrisScreenGrid = new Rectangle(40, 20, 340, 660);
+
+
+        //
+        // Block colors (might change, more to come)
+        //
+
+        black = rgb(0,0,0);
+
+        // Block texture
+        Texture blockTexture;
+        blockTexture = new Texture(Gdx.files.internal("block.png"));
+        blockTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+
+        blockTextureRegion = new TextureRegion(blockTexture, 1, 1, 34, 34);
+
+
     }
 
     // Convert RGB colors to this wiered system
     static Color rgb(int r, int g, int b){
         return new Color(r / 255f, g / 255f, b / 255f, 1.0f);
     }
+
+    // So X mark where there is a block and O where there isn't
+    // Will be updated with something like 1, 2, 3, 4 or something
+    // for different colored blocks
+    final static int X = 1;
+    final static int O = 0;
+
+    // Classical tetris shapes
+    // ( I-shape, O-shape, T-shape, L-shape, J-shape, Z-shape and S-shape)
+    // Stored upside down becasue the grid starts from bottom left
+    public static int[][][] tetrisPieces = new int[][][]{
+            {{O,X,O,O},
+             {O,X,O,O},
+             {O,X,O,O},
+             {O,X,O,O}},
+
+            {{X,X},
+             {X,X}},
+
+            {{O,X,O},
+             {X,X,X},
+             {O,O,O}},
+
+            {{O,X,X},
+             {O,X,O},
+             {O,X,O}},
+
+            {{X,X,O},
+             {O,X,O},
+             {O,X,O}},
+
+            {{X,O,O},
+             {X,X,O},
+             {O,X,O}},
+
+            {{O,O,X},
+             {O,X,X},
+             {O,X,O}}
+    };
 
 }
--- core/src/com/mygdx/tetrisoncrack/TetrisScreen.java
+++ core/src/com/mygdx/tetrisoncrack/TetrisScreen.java
@@ -6,21 +6,48 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.Rectangle;
 
 
 public class TetrisScreen implements Screen {
+
+    // The grid size mesaured in blocks
+    // based on Tetris standard size
+    // Rows above 20 is "off screen"
+    final int GRID_HEIGHT = 24;
+    final int GRID_WIDTH = 10;
+
+    // Block size is based on 10 blocks to fit in 340 px
+    // 340 px the width of the area of the tetrisScreenGrid
+    final int BLOCK_SIZE = 34;
 
     SpriteBatch batch = new SpriteBatch();
     Vector3 touchPoint = new Vector3();
 
     OrthographicCamera cam;
-    Sprite game_sprite;
+    Sprite game_sprite, block_sprite;
     Game game;
+    GameState state;
+
+    // Using different game states
+    // so we can pause the game for example
+    interface GameState {
+        void draw();
+        void update(float delta);
+    }
+
+
+    // The grid 2-dimensional array where origo is bottom left
+    // It's [row][column]
+    int[][] grid = new int[GRID_HEIGHT][GRID_WIDTH];
+
 
     public TetrisScreen(Game game) {
 
         this.game = game;
+        state = new PlayState();
 
         cam = new OrthographicCamera();
         cam.setToOrtho(false, 480, 800);
@@ -28,6 +55,54 @@ public class TetrisScreen implements Screen {
         game_sprite = new Sprite(Ass.gameScreen);
         game_sprite.setPosition(0, 0);
 
+        block_sprite = new Sprite(Ass.blockTextureRegion);
+        block_sprite.setPosition(0, 0);
+
+        // JUST FOR TESTING: Added some random blocks
+        grid[5][5] = 1;
+        grid[5][6] = 1;
+        grid[5][7] = 1;
+        grid[6][6] = 1;
+
+
+    }
+
+    class PlayState implements GameState {
+
+        public void draw(){
+            drawGame();
+        }
+
+        public void update(float delta){
+            if(pushed(Ass.gamePauseButton)){
+                state = new PauseState(PlayState.this);
+            }
+        }
+    }
+
+    class PauseState implements GameState {
+        GameState returnState;
+        public PauseState(GameState returnState){
+            this.returnState = returnState;
+        }
+
+        public void draw(){
+            batch.draw(Ass.pauseScreen, 0, 0);
+        }
+
+        public void update(float delta){
+
+            // Should get it's own button in the future
+            // now it's just down in the corner because of reasons
+            if(pushed(Ass.gamePauseButton)){
+                state = returnState;
+            }
+
+            if(pushed(Ass.pauseScreenMenuButton)){
+               game.setScreen(new MenuScreen(game));
+            }
+
+        }
     }
 
     // We have the buttons on the background so we make a clickable rectangle
@@ -44,21 +119,49 @@ public class TetrisScreen implements Screen {
     @Override
     public void render(float delta) {
 
-        // We have the buttons on the background so we make a clickable rectangle
-        // May implement Stage, Actors and event listeners?
-        if (pushed(Ass.gamePauseButton)){
-            game.setScreen(new PauseScreen(game));
-        }
-
         batch.setProjectionMatrix(cam.combined);
         batch.begin();
 
-        batch.draw(game_sprite, 0, 0);
-
+        state.draw();
+        state.update(delta);
 
         batch.end();
 
     }
+
+    void drawGame(){
+
+        batch.draw(game_sprite,  0,  0);
+
+        //drawBlocks(grid, Ass.tetrisScreenGrid, 0, 0);
+
+    }
+
+    void drawBlocks(int[][] blocks, Rectangle gridRectangle, int x, int y){
+
+        Matrix4 grid_scale = new Matrix4();
+        grid_scale.setToTranslationAndScaling(gridRectangle.x, gridRectangle.y, 0.0f, gridRectangle.width / BLOCK_SIZE, gridRectangle.height / BLOCK_SIZE, 1.0f);
+
+        batch.setTransformMatrix(grid_scale);
+
+        batch.setColor(Ass.black);
+
+        for(int i = 0; i < blocks.length; i++){
+            for(int j = 0; j < blocks[i].length; j++){
+                // Need to be expanded to take more colors than 1
+                if(blocks[i][j] == 1 && y + i < GRID_HEIGHT - 2){
+                    batch.draw(
+                            block_sprite,
+                            BLOCK_SIZE * (x + j),
+                            BLOCK_SIZE * (y + i));
+                }
+            }
+        }
+
+        batch.setTransformMatrix(new Matrix4().idt());
+    }
+
+
 
     @Override
     public void resize(int width, int height) {
